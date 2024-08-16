5a8991a1d90e8736392247e2f938f0bf6c5b598c
blaycoder <onatola4real@gmail.com>
2024-08-16 22:50:33 +0100

    made changes

--- app/hospitals/[hospitalID]/page.tsx
+++ app/hospitals/[hospitalID]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import HospitalDetails from "../../components/HospitalDetails";
 
 const HospitalPage = () => {
-  <h1>Rendering Homepage</h1>
+  <h1>Hospital Details</h1>
   return <HospitalDetails />;
 };
 
