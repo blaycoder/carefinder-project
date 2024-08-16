8ad6b330728ec4c1f7f99464c1b025e5cbad4d53
blaycoder <onatola4real@gmail.com>
2024-08-17 00:13:51 +0100

    rename firebase config variable

--- app/config/firebase.tsx
+++ app/config/firebase.tsx
@@ -7,23 +7,14 @@ import {
   Auth,
 } from "firebase/auth";
 
-const API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
-const AUTH_DOMAIN = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
-const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
-const STORAGE_BUCKET = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
-const MESSAGING_SENDER_ID =
-  process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
-const APP_ID = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
-const MEASUREMENT_ID = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
-
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
